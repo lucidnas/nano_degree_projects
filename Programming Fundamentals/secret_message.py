e2fa83fb46b821248ed98434df8057747296b835
amohammed <amohammed@logicworks.net>
2017-08-05 23:53:08 -0400

    secret program

--- Programming Fundamentals/secret_message.py
+++ Programming Fundamentals/secret_message.py
@@ -3,20 +3,16 @@ import random
 
 fd = r"C:/alphabet/organized"
 os.chdir(fd)
+#print os.getcwd()
 file_list = os.listdir(fd)
-print file_list
-for filename in file_list:
-    os.rename(filename, str(random.randrange(40)) + filename)
-print os.getcwd()
 
 
+def create_secret():
+    for filename in file_list:
+        os.rename(filename, str(random.randrange(40)) + filename)
+#create_secret()
 
-
-
-##print [''.join(),i)) for i in file_list]
-
-##
-##def organize_files:
-##    for filename in file_list:
-##        os.rename(filename, filename.transtlate(None, "1..9")
-##    return
+def reveal_secret():
+    for filename in file_list:
+        os.rename(filename, filename.translate(None, "0123456789"))
+reveal_secret()
