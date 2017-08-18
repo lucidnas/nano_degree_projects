68eab423969c6a512d4a215a50bf3fcd417d146f
amohammed <amohammed@logicworks.net>
2017-08-18 14:44:39 -0400

    update profanity

--- programming_fundamentals/profanity_chcker.py
+++ programming_fundamentals/profanity_chcker.py
@@ -1,8 +1,14 @@
-import os
+import urllib
 
 def read_text():
     quotes = open('C:\Python\movies_quotes.txt')
-    print(quotes.read())
+    quote = quotes.read()
+    print quote
     quotes.close()
+    check_profanity(quote)
 
+def check_profanity(text):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text)
+    print(connection.read())
+    connection.close()
 read_text()
